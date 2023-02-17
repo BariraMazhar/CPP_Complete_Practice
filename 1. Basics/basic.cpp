c91760b5669b09ad9d70308e91ec1d4f91428e01
Barira-Mazhar <bmmcpk@gmail.com>
2023-02-17 09:04:15 -0800

    For loop in c++

---
+++ 1. Basics/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum;
+   cout<<"Enter 1st number: "<<endl;
+   cin>> a;
+   cout<<"Enter 2nd number: "<<endl;
+   cin>> b ;
+  sum=a+b;
+  cout<<"sum of two numbers = "<<sum;
+  return 0;
+
+}
